2de98068ce56dc77c5991f52c97b752add1b6865
Adil-M2004 <D00265739@student.dkit.ie>
2025-02-28 01:17:49 +0000

    Reading from csv file

--- main.cpp
+++ main.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
-#include <string>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+
 using namespace std;
 
+// Struct representing a car STAGE 2
+struct Car {
+    int id;
+    string make;
+    string model;
+    int year;
+    float engine_size;
+};
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
-{
-    ifstream iFile;
-    string line = "";
-    string first = "";
-    int location = 0;
+// Function to read CSV into a vector of Car structs STAGE 2
+void loadCars(vector<Car>& cars, const string& filename) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Car car;
+        ss >> car.id; ss.ignore();
+        getline(ss, car.make, ',');
+        getline(ss, car.model, ',');
+        ss >> car.year; ss.ignore();
+        ss >> car.engine_size;
+        cars.push_back(car);
+    }
+    file.close();
+}
 
-    iFile.open("cars.csv");
-    getline(iFile, line);
-    getline(iFile, line);
-
-    location = line.find(',');
-    first = line.substr(0, location);
-    line = line.substr(location, line.length());
-    cout << line << endl;
-
-    return 0;
-
+// Function to display car data STAGE 3
+void displayCars(const vector<Car>& cars) {
+    cout << left << setw(6) << "ID"
+         << setw(12) << "Make"
+         << setw(12) << "Model"
+         << setw(8) << "Year"
+         << setw(12) << "Engine Size" << '\n';
+    cout << "--------------------------------------------------\n";
+    for (const auto& car : cars) {
+        cout << setw(6) << car.id
+             << setw(12) << car.make
+             << setw(12) << car.model
+             << setw(8) << car.year
+             << setw(12) << fixed << setprecision(1) << car.engine_size << '\n';
+    }
 }
 
 
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+int main() {
+    vector<Car> cars;
+    loadCars(cars, "cars.csv");
+    return 0;
+}
